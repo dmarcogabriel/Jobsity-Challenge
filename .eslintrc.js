28c38fe370644883214824dc8d7f43c161b94787
Gabriel De Marco <lino.dmarco@gmail.com>
2023-07-20 14:08:20 -0300

    fix: env variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
   extends: '@react-native',
   rules: {
     'react/react-in-jsx-scope': 0,
+    curly: 0,
   },
 };
--- src/config/api.ts
+++ src/config/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import Config from 'react-native-config'
+import Config from 'react-native-config';
 
 const api = axios.create({baseURL: Config.API_BASE_URL});
 
